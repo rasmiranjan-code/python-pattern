d3bde925abdd988fa22ac4c03aacbe6513a2ef75
Rasmi Ranjan Jena <rasmijena516@gmail.com>
2026-07-03 22:50:18 +0530

    Added program6

---
+++ program6.py
@@ -0,0 +1,10 @@
+#Write a Python program to convert kilometers to miles.
+kilometrs = float(input("Enter distance in kilometers: "))
+#conversion_factor = 0.621371
+miles = kilometrs * 0.621371
+print(f"{kilometrs} kilometers is equal to {miles} miles.")
+
+#write a python program to convert kilometers to meters
+kilometrs = float(input("Enter distance in kilometers: "))
+meters = kilometrs * 1000
+print(f"{kilometrs} kilometers is equal to {meters} meters.")
