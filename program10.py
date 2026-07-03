11ef9e0bcdb9e48e5be4daa3525588b3a6ec6286
Rasmi Ranjan Jena <rasmijena516@gmail.com>
2026-07-03 23:31:31 +0530

    Addded program10

---
+++ program10.py
@@ -0,0 +1,10 @@
+a = int(input("Enter value for a: "))
+b = int(input("Enter value for b: "))
+
+
+# swapping the values of a and b
+a, b = b, a
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
